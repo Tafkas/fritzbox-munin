78a5a202cf78290220d2ab2af2b4442a3bb7b9ae
Christian Stade-Schuldt <tafkasorg@yahoo.de>
2017-09-06 22:41:27 +0200

    move power consumption to system group

--- fritzbox_power_consumption.py
+++ fritzbox_power_consumption.py
@@ -44,7 +44,7 @@ def get_power_consumption():
 def print_config():
     print "graph_title AVM Fritz!Box Power Consumption"
     print "graph_vlabel %"
-    print "graph_category network"
+    print "graph_category system"
     print "graph_order system cpu wifi dsl ab usb"
     print "system.label system"
     print "system.type GAUGE"
